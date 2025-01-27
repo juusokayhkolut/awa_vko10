2038c514e90822b58c245d0842911a0dda8af124
Juuso Kayhko <juusookayhko@gmail.com>
2025-01-27 17:34:38 +0200

    new commit

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -15,7 +15,7 @@ const About: React.FC = () => {
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const response = await fetch('https://jsonplaceholder.typicode.com/post');
         const data = await response.json();
         setPosts(data);
       } catch (err) {
